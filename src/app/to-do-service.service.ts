5ecfb4b76691a21aa2c1ea1fb5913666f2325d61
mkeeni <mohnish_201900167@smit.smu.edu.in>
2021-11-12 20:56:18 +0530

    minor changes

--- src/app/to-do-service.service.ts
+++ src/app/to-do-service.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class ToDoServiceService {
   ToDoDetails = [
-    { "task": "SE Assignment", "id": "1.", "date":"13/11","time": "5pm" },
+    { "task": "Maths Assignment", "id": "1.", "date":"13/11","time": "5pm" },
     { "task": "SL Lab Work", "id": "2.", "date": "13/11","time": "11:59pm" },
     { "task": "OS Lab Work", "id": "3.", "date": "15/11","time": "10am" },
     { "task": "Online Conference", "id": "4.", "date": "20/11", "time": "6pm" },
-    { "task": "Python Code Challenge", "id": "5.", "date": "27/11", "time": "11:59pm" }
+    { "task": "Interview", "id": "5.", "date": "27/11", "time": "11:00am" }
   ]
 
   getToDo() {
